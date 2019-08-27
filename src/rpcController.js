afde0385bbbe7ad212342b29ea4d56ae3542170b
Maciej Krüger <mkg20001@gmail.com>
2019-08-27 14:03:02 +0200

    feat: rpc stubs

--- src/rpcController.js
+++ src/rpcController.js
@@ -1,7 +1,14 @@
 'use strict'
 
 async function RPCController ({swarm}) {
+  return {
+    blockRequest: async (parameters) => { // TODO
 
+    },
+    subscribe: async (chainId, {onEvent}) => {
+
+    }
+  }
 }
 
 module.exports = RPCController
